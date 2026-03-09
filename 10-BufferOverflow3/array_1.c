c16760087297328242644557a9c1f6fd638c2a5f
epika <epika025@gmail.com>
2026-03-09 18:54:26 +0900

    bof3

---
+++ 10-BufferOverflow3/array_1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    /*要素数を変更してください*/
+    char name[] = "masaru";
+
+    printf("%s\n",name);
+
+    return 0;
+}
---
+++ 10-BufferOverflow3/array_2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    char password[8];
+
+    /*strcpy関数を使用して文字列を代入してください*/
+    
+
+    if(strcmp(password,"DotExe") == 0)
+    {
+        printf("Excellent!!!");
+    }
+
+    return 0;
+}
